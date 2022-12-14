7c7bc2354946c8ed3bd15d782802800739d2d13e
Anton Maksimenkov <urbangorod888@gmail.com>
2022-12-14 20:48:10 +0300

    completed with media 768px

--- block7/src/js/index.js
+++ block7/src/js/index.js
@@ -1,4 +1,12 @@
 import '../scss/style.scss';
 import menu_popup_call from './menu_popup_call';
 import menu_popup_feedback from './menu_popup_feedback';
+import readMore from './readMore';
+import readMore1 from './readMore1';
+import readMore2 from './readMore2';
+import menu_popup from './menu_popup';
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// const swiper = new Swiper('.swiper', {
+//     modules: [Navigation, Pagination],
+//   });
 console.log('Works!');
