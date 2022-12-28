055ba3aa798a5eec7d8df5dec10683e8131fb78d
Anton Maksimenkov <urbangorod888@gmail.com>
2022-12-28 13:50:44 +0300

    making amendments

--- block7/src/js/menu_popup.js
+++ block7/src/js/menu_popup.js
@@ -1,13 +1,16 @@
 let callButtonOpen = document.querySelector('.menu__media__burger')
 let callMenu = document.querySelector('.menu-popup__container')
 let callButtonClose = document.querySelector('.menu-popup__close')
+let noScroll = document.querySelector('body')
 
 callButtonOpen.addEventListener('click', function(){
     callButtonOpen.classList.toggle('active');
     callMenu.classList.toggle('active');
+    noScroll.classList.toggle('no-scroll');
 })
 
 callButtonClose.addEventListener('click', function(){
     callButtonClose.classList.toggle('active');
     callMenu.classList.toggle('active');
+    noScroll.classList.toggle('no-scroll');
 })
