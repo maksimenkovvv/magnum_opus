936578cab7affaffa9246aa6c9008a7ee6b7e241
Anton Maksimenkov <urbangorod888@gmail.com>
2022-12-28 17:25:33 +0300

    ammedments

--- block7/src/js/menu_popup_call.js
+++ block7/src/js/menu_popup_call.js
@@ -1,13 +1,16 @@
 let callButtonOpen = document.querySelector('.menu__contacts__icons-call')
 let callMenu = document.querySelector('.menu__popup-call')
 let callButtonClose = document.querySelector('.menu__popup-call__close')
+let noScroll = document.querySelector('body')
 
 callButtonOpen.addEventListener('click', function(){
     callButtonOpen.classList.toggle('active');
     callMenu.classList.toggle('active');
+    noScroll.classList.toggle('no-scroll');
 })
 
 callButtonClose.addEventListener('click', function(){
     callButtonClose.classList.toggle('active');
     callMenu.classList.toggle('active');
+    noScroll.classList.toggle('no-scroll');
 })
--- block7/src/js/menu_popup_feedback.js
+++ block7/src/js/menu_popup_feedback.js
@@ -1,13 +1,16 @@
 let callButtonOpen = document.querySelector('.menu__contacts__icons-chat')
 let callMenu = document.querySelector('.menu__popup-feedback')
 let callButtonClose = document.querySelector('.menu__popup-feedback__close')
+let noScroll = document.querySelector('body')
 
 callButtonOpen.addEventListener('click', function(){
     callButtonOpen.classList.toggle('active');
     callMenu.classList.toggle('active');
+    noScroll.classList.toggle('no-scroll');
 })
 
 callButtonClose.addEventListener('click', function(){
     callButtonClose.classList.toggle('active');
     callMenu.classList.toggle('active');
+    noScroll.classList.toggle('no-scroll');
 })
